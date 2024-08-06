e2e6721a32ad69aa8d8078bed119545f283258da
Yottaline <yottaline@gmail.com>
2024-08-06 10:29:18 +0300

    update configurations

--- resources/views/website/pages/home.blade.php
+++ resources/views/website/pages/home.blade.php
@@ -33,12 +33,14 @@
             flex-basis: 10%;
             -webkit-filter: grayscale(100%);
             filter: grayscale(100%);
-            transition: all ease-in-out 400ms;
+            opacity: .8;
+            transition: all ease-in-out 800ms;
             cursor: pointer;
         }
 
         .box.active {
             flex-grow: 1;
+            opacity: 1;
             -webkit-filter: grayscale(0);
             filter: grayscale(0);
         }
