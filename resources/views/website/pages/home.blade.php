34cbf50468fb7ea5fdcd8ac85cd98c73fc371ed5
Yottaline <yottaline@gmail.com>
2024-08-07 00:35:48 +0300

    2

--- resources/views/website/pages/home.blade.php
+++ resources/views/website/pages/home.blade.php
@@ -41,7 +41,7 @@
             <div class="row">
                 <div class="col-12 col-sm-6 d-flex flex-column">
                     <p class="my-auto py-auto">
-                        Children’s Runway Paris brings together the world's leading children’s fashion brands in one
+                        Children’s Runway brings together the world's leading children’s fashion brands in one
                         spectacular
                         event.
                         Hosted in the heart of Paris, the global capital of fashion, this tradeshow promises to be a
